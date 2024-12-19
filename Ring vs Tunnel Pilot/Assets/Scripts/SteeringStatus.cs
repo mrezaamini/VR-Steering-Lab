67959eedafa2efbd1ef623d38e087fca3df3f334
Mohammadreza Amini <45162458+mrezaamini@users.noreply.github.com>
2024-12-19 14:44:02 -0500

    steering endpoits

---
+++ Ring vs Tunnel Pilot/Assets/Scripts/SteeringStatus.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteeringStatus : MonoBehaviour
+{
+    public GameManager gameManager;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.parent == this.transform)
+        {
+            return;
+        }
+
+     
+            Debug.Log("otherr collider " + other.gameObject.tag);
+            Debug.Log("thiss collider " + gameObject.tag);
+            //if (other.gameObject.CompareTag("InsideRing"))
+            //{
+            //    gameManager.OnStartTraversing();
+            //}
+            //else if (other.gameObject.CompareTag("EndPoint"))
+            //{
+            //    gameManager.OnFinishTraversing();
+            //}
+
+    }
+}
