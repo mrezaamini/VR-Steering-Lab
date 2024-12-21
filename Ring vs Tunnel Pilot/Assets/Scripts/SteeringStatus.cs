d253b0e47bcd26038dbe373353cfef3b5a2b73f3
Mohammadreza Amini <45162458+mrezaamini@users.noreply.github.com>
2024-12-20 22:38:32 -0500

    RW-condition management

--- Ring vs Tunnel Pilot/Assets/Scripts/SteeringStatus.cs
+++ Ring vs Tunnel Pilot/Assets/Scripts/SteeringStatus.cs
@@ -5,6 +5,19 @@ using UnityEngine;
 public class SteeringStatus : MonoBehaviour
 {
     public GameManager gameManager;
+
+    void Start()
+    {
+        if (gameManager == null)
+        {
+            gameManager = FindObjectOfType<GameManager>();
+            if (gameManager == null)
+            {
+                Debug.LogError("GameManager reference not found!");
+            }
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.parent == transform.parent)
@@ -13,17 +26,20 @@ public class SteeringStatus : MonoBehaviour
         }
         if (other.gameObject.CompareTag("StartPoint"))
         {
+            Debug.Log("Startedddd, YAYYYY");
             gameManager.OnStartTraversing();
+            
         }
         if (other.gameObject.CompareTag("EndPoint"))
         {
-            gameManager.OnFinishTraversing();
+            Debug.Log("ENDEDD, YAYYYY");
+            gameManager.EndTrial();
         }
 
         
 
      
-            //Debug.Log("otherr collider " + other.gameObject.tag);
+            //Debug.Log("otherr collider " + other.gameObject.name);
             //Debug.Log("thiss collider " + gameObject.tag);
             //if (other.gameObject.CompareTag("InsideRing"))
             //{
